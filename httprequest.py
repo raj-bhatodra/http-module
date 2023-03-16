e075e0674183335f410aa389e71f3900a1e3fb3d
Raj Bhatodra <devcenter14@gmail.com>
2023-03-17 02:37:51 +0530

    edit file

--- httprequest.py
+++ httprequest.py
@@ -1,6 +1,6 @@
 import requests
 
-response = requests.get('https://www.ample.com')
+response = requests.get('https://www.google.com')
 if response.status_code == 200:
 	print(f'HTTP requests working status_code: {response.status_code}')
 else:
