9e6461ab966d8821485a92b567616e9686e8644b
Raj Bhatodra <devcenter14@gmail.com>
2023-03-17 02:24:23 +0530

    First Commit

---
+++ httprequest.py
@@ -0,0 +1,7 @@
+import requests
+
+response = requests.get('https://www.ample.com')
+if response.status_code == 200:
+	print(f'HTTP requests working status_code: {response.status_code}')
+else:
+	print('Error:', response.status_code)
